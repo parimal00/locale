5e040a512279adab23300a4d8dbee29cf7edd1ac
parimal00 <parimalbhattarai00@gmail.com>
2022-04-18 00:39:47 -0400

    locale completed

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<div style="display: flex;justify-content:center;align-items:center;flex-direction:column">
+<h1>{{__('profile.welcome')}}</h1>
+
+<div>
+    <a href="">{{__('profile.about')}}</a>
+    <a href="">{{__('profile.contact')}}</a>
+    <a href="">{{__('profile.list')}}</a>
+</div>
+<div>
+    <br>
+    <br>
+  
+    <a href="/profile/en">Change to english</button>
+    <a href="/profile/ne">नेपालीमा परिवर्तन</button>
+
+</div>
+</div>
